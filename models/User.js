3a023e2fcda364e69bac45b6612cff89312dc886
Medlazhar <121746533+Medlazhar@users.noreply.github.com>
2026-03-19 22:11:05 +0100

    Add new fields to User schema

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const dataSchema = new mongoose.Schema({
   Username: String,
   Password: String,
   isAdmin: String,
+  name:String,
+  birthday:String,
+  phone:String,
+  adress:String
   
 });
 
