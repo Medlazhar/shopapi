8cb78d40e3c4f00e67f5f08302a9ee19132b5ab0
Medlazhar <121746533+Medlazhar@users.noreply.github.com>
2026-03-22 17:35:31 +0100

    Add new_message model for message storage

--- models/new_message.js
+++ models/new_message.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose")
+const sending_msg = new mongoose.Schema({
+Username:String,
+msg_date:String,
+  msg:String
 
+})
+
+module.exports=mongoose.model("msg",sending_msg,"messages");
