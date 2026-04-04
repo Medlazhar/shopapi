0ef13b53437b51c9463c3310f52a2ceb83ff3e61
Medlazhar <121746533+Medlazhar@users.noreply.github.com>
2026-04-05 05:49:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post("/update_record",async (req,res)=>{
 
   const {rec_id,rec_user,rec_pass,rec_birth,rec_adress,rec_phone,rec_name,rec_isAdmin} = req.body;
 
-  const update_value = await op.findById(rec_id)
+  const update_value = await op.findById({_id:rec_id})
   if(update_value){
  update_value.Username= rec_user;
     update_value.Password= rec_pass;
